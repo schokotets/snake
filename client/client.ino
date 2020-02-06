a6250fa69f4de02a245da8f92d31f1c46aaa9f93
schokotets <moritzleni@gmail.com>
2020-02-06 10:07:18 +0100

    simple arduino client

---
+++ client/client.ino
@@ -0,0 +1,36 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+char ssid* = "snake"; // network SSID (name)
+char pass* = "n3tw0rk!"; // network password
+unsigned int udpPort = 9900; // local port to listen for UDP packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+void setup() {
+  Serial.begin(38400);
+  WiFi.begin(ssid, pass);
+  
+  while ( WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  Serial.println("[setup] WiFi connected");
+  Serial.print("[setup] IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  udp.begin(udpPort);
+}
+
+void loop() {
+  IPAddress broadcastIp = WiFi.localIP();
+  broadcastIp[3] = 255;
+  Serial.println(broadcastIp);
+  udp.beginPacket(broadcastIp, udpPort);
+  udp.print("1,1");
+  udp.endPacket();
+  
+  delay(2000);
+}
