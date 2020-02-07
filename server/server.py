c84421b8ccec24f7e80981e13b51b6c412a9c7ae
Moritz Schulz <moritzleni@gmail.com>
2020-02-07 09:34:36 +0100

    parse received udp data

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ import threading
 import http.server
 import socketserver
 import socket
+import struct
 
 HTTP_PORT=8080
 UDP_PORT=9900
@@ -24,7 +25,11 @@ class UdpThread(threading.Thread):
 
         while True:
             data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-            print("received message:", data)
+            if(len(data) == 3):
+                id,x,y = struct.unpack("Bbb", data[:3])
+                print("received id:", id, "x:", x, "y:", y)
+            else:
+                print("received message of invalid length", len(data))
 
 HttpThread().start()
 UdpThread().start()
